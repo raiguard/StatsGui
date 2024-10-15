3ff1f416fd8e143ff44e42c0a89e63961e930e50
Caleb Heuer <ch@raiguard.me>
2024-10-14 21:36:39 -0600

    Account for ticks_per_day in days calculation
    Fixes #3

--- scripts/sensor/daytime.lua
+++ scripts/sensor/daytime.lua
@@ -1,14 +1,18 @@
+local flib_format = require("__flib__.format")
+
+--- @param player LuaPlayer
 return function(player)
   if not storage.players[player.index].settings.show_daytime then
     return
   end
 
-  local days = math.floor(1 + ((game.tick + 12500) / 25000))
-
   local daytime = player.surface.daytime + 0.5
   local daytime_minutes = math.floor(daytime * 24 * 60)
   local daytime_hours = math.floor(daytime_minutes / 60) % 24
   daytime_minutes = daytime_minutes - (daytime_minutes % 15)
+
+  local ticks_per_day = player.surface.ticks_per_day
+  local days = math.floor(1 + ((game.tick + (ticks_per_day / 2)) / ticks_per_day))
 
   return {
     "",
@@ -16,6 +20,6 @@ return function(player)
     " = " .. string.format("%d:%02d", daytime_hours, daytime_minutes % 60),
     ", ",
     { "statsgui.day" },
-    " " .. days,
+    " " .. flib_format.number(days),
   }
 end
